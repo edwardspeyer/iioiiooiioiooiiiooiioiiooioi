050746d7660de5fa3108f84488323bf726b1a9a0
EGS <edwardspeyer@users.noreply.github.com>
2021-11-10 15:10:45 +0000

    red vs blue

--- life.js
+++ life.js
@@ -28,6 +28,7 @@ const _ = null;
 const I = 'i';
 const O = 'o';
 
+
 function build(grid, size) {
   let cells = [];
   let [width, height] = size;
@@ -43,18 +44,58 @@ function build(grid, size) {
   return cells;
 }
 
+
+function random(state0) {
+  let state1 = state0.map(v => {
+    if (v) {
+      return v;
+    } else if (Math.random() <= 0.3) {
+      return I;
+    } else if (Math.random() >= 0.7) {
+      return O;
+    }
+  });
+  return state1;
+}
+
+
+function drop(team, state0, size) {
+  const density = 0.3
+
+  let [width, height] = size;
+  let r = n => Math.floor(Math.random() * n)
+  let origin_x = r(width);
+  let origin_y = r(height);
+  let radius = width / 10;
+
+  for (let y = origin_y - radius; y < origin_y + radius; y++) {
+    for (let x = origin_x - radius; x < origin_x + radius; x++) {
+      let r2 = (x - origin_x) ** 2 + (y - origin_y) ** 2;
+      if (r2 <= radius ** 2) {
+        if (Math.random() <= density) {
+          if (x >= 0 && x < width && y >= 0 && y < height) {
+            let i = x + y * width;
+            state0[i] = team;
+          }
+        }
+      }
+    }
+  }
+}
+
+
 function render(state, cells) {
   state.forEach((v, i) => {
     if (v) {
-      cells[i].style = 'background: #777;'
+      cells[i].classList = v;
     } else {
-      cells[i].style = '';
+      cells[i].className = '';
     }
   });
   return state;
 }
 
-function tick(state0, size) {
+function next(state0, size) {
   let [width, height] = size;
   let state1 = state0.map(() => null);
 
@@ -64,20 +105,27 @@ function tick(state0, size) {
     let n = {};
     Offsets.forEach(pair => {
       let [dx, dy] = pair;
-      let j = (x + dx) + (y + dy) * width;
-      let s = state0[j] || null;
-      n[s] = (n[s] || 0) + 1;
+      let px = x + dx, py = y + dy;
+      if (px >= 0 && px < width && py >= 0 && py < height) {
+        let j = px + py * width;
+        let s = state0[j] || null;
+        n[s] = (n[s] || 0) + 1;
+      }
     });
 
+    // Rules!
+    let c = state0[i];
     if (state0[i]) {
-      if (n[I] >= 2 && n[I] <= 3) {
-        state1[i] = I;
+      if (n[c] >= 2 && n[c] <= 3) {
+        state1[i] = c;
       } else {
         state1[i] = null;
       }
     } else {
       if (n[I] == 3) {
         state1[i] = I;
+      } else if (n[O] == 3) {
+        state1[i] = O;
       }
     }
   }
@@ -85,22 +133,26 @@ function tick(state0, size) {
   return state1;
 }
 
-function random(state0, density) {
-  let state1 = state0.map(v => {
-    if (v) {
-      return v;
-    } else if (Math.random() <= density) {
-      return I;
-    }
-  });
-  return state1;
+let size = [30, 30];
+let cells = build(grid, size);
+let state = cells.map(() => null);
+state = random(state);
+
+render(state, cells);
+
+let time = 0;
+
+function tick() {
+  if (time % 12 == 0) {
+    let team;
+    if (Math.random() <= 0.5) team = I;
+    else team = O;
+    drop(team, state, size);
+  }
+  state = next(state, size);
+  render(state, cells);
+  time += 1;
 }
 
 
-let size = [60, 60];
-let cells = build(grid, size); 
-let state = cells.map(() => null);
-state = random(state, 0.5);
-render(state, cells);
-
-window.setInterval(() => state = render(tick(state, size), cells), 1000/24);
+window.setInterval(tick, 1000/24);
